82bb38e59ba6b9f8ad84b725ef3d67b01d420236
Kevin Corey <kevinkaua10.jau@gmail.com>
2024-07-09 20:20:07 -0300

    Configuração do middleware de json do express

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import 'dotenv/config'
 import express from 'express'
 import { teste } from './controladores/controladores'
+import rotas from './rotas'
+import { json } from 'express'
 
 const app = express()
 
-app.get('/', teste)
+app.use (express.json())
+
+app.use(rotas)
 
 app.listen(process.env.PORT)
