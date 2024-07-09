970a74aa5ba5eb6b2c6f917aa82c04def73e499b
Kevin Corey <kevinkaua10.jau@gmail.com>
2024-07-09 20:15:39 -0300

    estrutura da api

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import 'dotenv/config'
 import express from 'express'
+import { teste } from './controladores/controladores'
 
 const app = express()
 
-app.get('/', (req, res) => {
-    return res.send ('OK!')
-})
+app.get('/', teste)
 
 app.listen(process.env.PORT)
